520a577a5efa11a755e96f31beb5b7e26794853a
Robert van Braam <12235691+RobertvBraam@users.noreply.github.com>
2023-12-17 11:03:47 +0900

    Setup ci pipeline and made sure tests run in linux OS

--- src/Integration.Tests/Utils/TestAppHelper.cs
+++ src/Integration.Tests/Utils/TestAppHelper.cs
@@ -1,7 +1,10 @@
-﻿namespace Integration.Tests.Utils;
+﻿using System.Runtime.InteropServices;
+
+namespace Integration.Tests.Utils;
 
 public static class TestAppHelper
 {
     public static string CurrentDirectory => Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.ToString();
-    public static string TestAppDirectory => CurrentDirectory + @"\TestApp\";
+    public static string TestAppDirectory => CurrentDirectory + @"/TestApp/";
+    public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 }
